9f464046fed78de851053b2a40a47550a4b71555
andrico1234 <andrico1234@users.noreply.github.com>
2021-10-28 19:50:49 +0000

    Update platform deliverables

--- output/ios/StyleDictionarySize.h
+++ output/ios/StyleDictionarySize.h
@@ -2,7 +2,7 @@
 // StyleDictionarySize.h
 //
 // Do not edit directly
-// Generated on Thu, 28 Oct 2021 19:42:59 GMT
+// Generated on Thu, 28 Oct 2021 19:50:49 GMT
 //
 
 #import <Foundation/Foundation.h>
--- output/ios/StyleDictionarySize.m
+++ output/ios/StyleDictionarySize.m
@@ -3,7 +3,7 @@
 // StyleDictionarySize.m
 //
 // Do not edit directly
-// Generated on Thu, 28 Oct 2021 19:42:59 GMT
+// Generated on Thu, 28 Oct 2021 19:50:49 GMT
 //
 
 #import "StyleDictionarySize.h"
