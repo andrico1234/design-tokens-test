7563ea1d02e8486f910b220bcdccefb2e537b3b7
Andrico <17087167+andrico1234@users.noreply.github.com>
2021-10-28 08:29:29 +0100

    update config

--- output/ios/StyleDictionaryColor.m
+++ output/ios/StyleDictionaryColor.m
@@ -3,7 +3,7 @@
 // StyleDictionaryColor.m
 //
 // Do not edit directly
-// Generated on Wed, 27 Oct 2021 21:57:27 GMT
+// Generated on Thu, 28 Oct 2021 07:29:10 GMT
 //
 
 #import "StyleDictionaryColor.h"
