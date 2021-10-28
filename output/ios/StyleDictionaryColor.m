5df0dd6b64230f808b42f63cbfe2814452e07920
Andrico <17087167+andrico1234@users.noreply.github.com>
2021-10-28 20:20:32 +0100

    output to js

--- output/ios/StyleDictionaryColor.m
+++ output/ios/StyleDictionaryColor.m
@@ -3,7 +3,7 @@
 // StyleDictionaryColor.m
 //
 // Do not edit directly
-// Generated on Thu, 28 Oct 2021 07:29:10 GMT
+// Generated on Thu, 28 Oct 2021 19:20:15 GMT
 //
 
 #import "StyleDictionaryColor.h"
--- output/ios/StyleDictionarySize.h
+++ output/ios/StyleDictionarySize.h
@@ -2,7 +2,7 @@
 // StyleDictionarySize.h
 //
 // Do not edit directly
-// Generated on Thu, 28 Oct 2021 07:29:10 GMT
+// Generated on Thu, 28 Oct 2021 19:20:15 GMT
 //
 
 #import <Foundation/Foundation.h>
