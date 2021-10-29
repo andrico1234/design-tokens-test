9e76c84b8c7b24790e7a4eaeea81fb84efbd05de
andrico1234 <andrico1234@users.noreply.github.com>
2021-10-29 15:59:44 +0000

    Update platform deliverables

--- output/ios/StyleDictionaryColor.h
+++ output/ios/StyleDictionaryColor.h
@@ -3,7 +3,7 @@
 // StyleDictionaryColor.h
 //
 // Do not edit directly
-// Generated on Fri, 29 Oct 2021 15:05:05 GMT
+// Generated on Fri, 29 Oct 2021 15:59:44 GMT
 //
 
 #import <UIKit/UIKit.h>
--- output/ios/StyleDictionarySize.h
+++ output/ios/StyleDictionarySize.h
@@ -2,7 +2,7 @@
 // StyleDictionarySize.h
 //
 // Do not edit directly
-// Generated on Fri, 29 Oct 2021 15:05:05 GMT
+// Generated on Fri, 29 Oct 2021 15:59:44 GMT
 //
 
 #import <Foundation/Foundation.h>
